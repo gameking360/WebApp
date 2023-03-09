e2c279d9f745b298749a13cefbe851bfb1815000
Labes <dalmarco.outlook.com>
2023-03-09 19:27:41 -0300

    MySQL adaptation and first migration

--- AppWeb/Startup.cs
+++ AppWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace AppWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<AppWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AppWebContext")));
+                    options.UseMySql(Configuration.GetConnectionString("AppWebContext"), builder =>
+                    builder.MigrationsAssembly("AppWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
