e40be41df7488f1dac125b0236f47f731e1d2997
Labes <dalmarco.outlook.com>
2023-03-11 00:34:22 -0300

    Foreign key not null

--- AppWeb/Migrations/20230311032921_DepartmentForeignKey.Designer.cs
+++ AppWeb/Migrations/20230311032921_DepartmentForeignKey.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AppWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppWeb.Migrations
 {
     [DbContext(typeof(AppWebContext))]
-    partial class AppWebContextModelSnapshot : ModelSnapshot
+    [Migration("20230311032921_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
