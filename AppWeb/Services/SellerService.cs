0a73d460246051eac927c7fd862320b7e2a545b8
Labes <dalmarco.outlook.com>
2023-03-11 12:00:50 -0300

    Seller details

--- AppWeb/Services/SellerService.cs
+++ AppWeb/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppWeb.Models;
 using AppWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppWeb.Services
 {
@@ -27,7 +28,7 @@ namespace AppWeb.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);            
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);            
         }
 
         public void Remove(int id)
