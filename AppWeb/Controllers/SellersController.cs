b7c6267a70767567680889d649752d78595b0b69
Labes <dalmarco.outlook.com>
2023-03-11 11:18:57 -0300

    SellerFormViewModel and Department select component

--- AppWeb/Controllers/SellersController.cs
+++ AppWeb/Controllers/SellersController.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppWeb.Services;
 using AppWeb.Models;
+using AppWeb.Models.ViewModels;
 
 namespace AppWeb.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         
         public IActionResult Index()
@@ -25,14 +28,16 @@ namespace AppWeb.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel {Departments = departments   };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller obj)
+        public IActionResult Create(Seller seller)
         {
-            _sellerService.Insert(obj);
+            _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
     }
---
+++ AppWeb/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppWeb.Data;
+using AppWeb.Models;
+
+namespace AppWeb.Services
+{
+    public class DepartmentService
+    {
+        private readonly AppWebContext _context;
+
+        public DepartmentService(AppWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(p => p.Name).ToList();
+        }
+    }
+}
