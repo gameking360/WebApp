061104bbae699b048dfae28c457256def8df9b9b
Labes <dalmarco.outlook.com>
2023-03-11 20:54:28 -0300

    Validation

--- AppWeb/Models/Seller.cs
+++ AppWeb/Models/Seller.cs
@@ -8,17 +8,29 @@ namespace AppWeb.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+
+
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Birthday { get; set; }
 
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
+        [Range(100.00,50000.00, ErrorMessage = "{0} must be from {1} to {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
