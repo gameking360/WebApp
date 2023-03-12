50b10e30524e45e044e3996a83c03b635aab9238
Labes <dalmarco.outlook.com>
2023-03-11 22:56:56 -0300

    Exceotion handling for delete

---
+++ AppWeb/Services/Exceptions/IntegrityExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AppWeb.Services.Exceptions
+{
+    public class IntegrityExceptions : ApplicationException
+    {
+        public IntegrityExceptions(string message) : base(message)
+        {
+
+        }
+    }
+}
