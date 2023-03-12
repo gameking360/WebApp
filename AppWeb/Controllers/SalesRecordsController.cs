877aa0b06b8644aff2d21c0c2b43f9a52a746070
Labes <dalmarco.outlook.com>
2023-03-12 00:31:28 -0300

    Implementing grouping search

--- AppWeb/Controllers/SalesRecordsController.cs
+++ AppWeb/Controllers/SalesRecordsController.cs
@@ -38,9 +38,22 @@ namespace AppWeb.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 01, 01);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordsService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
